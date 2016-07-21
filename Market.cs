bdbad37da629a69066f2ac9768c2172b237213f1
Jeff <jeffreycai@college.harvard.edu>
2016-07-21 11:03:09 -0400

    adsf

---
+++ Market.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace etc
+{
+	class Market
+	{
+		void Hello() { }
+		void Add(int id, string symbol, bool buy, int price, int size) { }
+		void Convert(int id, string symbol, bool buy, int size) { }
+		void Cancel(int id) { }
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,8 @@ namespace etc
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			var client = new TcpClient(args[1], int.Parse(args[2]));
+			var stream = client.GetStream();
 		}
 	}
 }
