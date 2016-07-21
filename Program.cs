a9eaf2b1c42798df987b0f252a498ee990a61c0f
Jeff <jeffreycai@college.harvard.edu>
2016-07-21 11:40:28 -0400

    afwwa

--- Program.cs
+++ Program.cs
@@ -12,7 +12,11 @@ namespace etc
 		static void Main(string[] args)
 		{
             Console.WriteLine("hello swj05652");
-			var client = new TcpClient(args[0], int.Parse(args[1]));
+			string host = args[0];
+			int port = int.Parse(args[1]);
+			Console.WriteLine(string.Format("Connecting to {0}:{1}", host, port));
+
+			var client = new TcpClient(host, port);
 			var stream = client.GetStream();
 			var market = new Market(stream);
 			market.Hello();
