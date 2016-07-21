80a9e9fa51d46ed444b8e6b7df6e54a799375a50
swj05652 <swj05652@gmail.com>
2016-07-21 23:55:54 +0800

    tu# with '#' will be ignored, and an empty message aborts the commit.

---
+++ BondMarketMaker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace etc
+{
+    class BondMarketMaker
+    {
+        public int cash;
+        public int bond;
+        public int order;
+
+        private Market market;
+
+        public BondMarketMaker(Market market_)
+        {
+            market = market_;
+            order = 0;
+        }
+
+        public int Main()
+        {
+            while (true)
+            {
+                market.Add(order, "BOND", Direction.BUY, 999, 20);
+                order++;
+                market.Add(order, "BOND", Direction.SELL, 1001, 20);
+                order++;
+            }
+            return 0;
+        }
+    }
+}
